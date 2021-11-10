ff0413375d900cdb09805e34ce8aeff7d7f5c0b4
grrttmrtn1 <49645329+grrttmrtn1@users.noreply.github.com>
2021-11-09 20:56:15 -0600

    Update poshhasher.ps1
    adding file and folder hash output

--- poshhasher.ps1
+++ poshhasher.ps1
@@ -1,32 +1,80 @@
-﻿$algorithm = 'Both'
-$file = 'C:\Users\grrtt\Desktop\CarParts.xlsx'
+param (
+    [string]$file, 
+    [string]$folder,   
+    [string]$search,
+    [ValidateSet("MD5","SHA256","Both")]
+    [string]$hashType = "Both",
+    [ValidateSet($false, $true)]
+    [bool]$verbose = $false,
+    [ValidateSet($false, $true)]
+    [bool]$recursive = $true,
+    [ValidateSet($false, $true)]
+    [bool]$debug = $false
+)
 
-if ($algorithm -eq 'Both') {
-    $Object = New-Object PSObject -Property @{
-        FileName = $file
-        MD5 = (Get-FileHash -Algorithm MD5 -Path $file).Hash
-        SHA256 = (Get-FileHash -Algorithm SHA256 -Path $file).Hash
+
+if ([string]::IsNullOrWhiteSpace($search))
+{
+    if (![string]::IsNullOrWhiteSpace($file))
+    {
+        if ($hashType -eq 'Both') {
+            $Object = New-Object PSObject -Property @{
+                FileName = $file
+                MD5 = (Get-FileHash -Algorithm MD5 -Path $file).Hash
+                SHA256 = (Get-FileHash -Algorithm SHA256 -Path $file).Hash
+                }
+                $Object
         }
-        $Object
-}
-else {
-    $Object = New-Object PSObject -Property @{ 
-        FileName = $file
-        $algorithm = (Get-FileHash -Algorithm $algorithm -Path $file).Hash
+        else {
+            $Object = New-Object PSObject -Property @{ 
+                FileName = $file
+                $hashType = (Get-FileHash -Algorithm $hashType -Path $file).Hash
+            }
+            $Object
+        }
     }
-    $Object
+    elseif (![string]::IsNullOrWhiteSpace($folder))
+    {
+        if ($hashType -eq 'Both') 
+        {
+            Get-ChildItem -Path $folder -Recurse -File| Where {! $_.PSIsContainer} |Select-Object @{name='FileName';Expression={($_.FullName)}}, @{Name='MD5';Expression={(Get-FileHash $_.FullName -Algorithm MD5).Hash}}, @{Name='SHA256';Expression={(Get-FileHash $_.FullName -Algorithm SHA256).Hash}}
+        }
+        else 
+        {
+            Get-ChildItem -Path $folder -Recurse -File| % {Get-FileHash $_.Fullname -Algorithm $hashType}
+        }
+    }
+    else 
+    {
+        throw 'Not enough input to arguments. Need a folder(-folder) or file(-file)'
+    }
+}
+else
+{
+    if (![string]::IsNullOrWhiteSpace($file))
+    {
+        
+    }
+    elseif (![string]::IsNullOrWhiteSpace($folder))
+    {
+    
+    }
+    else 
+    {
+        throw 'Not enough input to arguments. Need a folder(-folder) or file(-file)'
+    }
 }
 
 
 #Get-ChildItem -Path C:\Users\grrtt\Desktop | % {Get-FileHash -Algorithm MD5 -Path $_.Fullname; Get-FileHash -Algorithm SHA256 -Path $_.FUllName}
 #Get-ChildItem -Path C:\Users\grrtt\Desktop -Recurse | Where {! $_.PSIsContainer} |Select-Object @{name='FileName';Expression={($_.FullName)}}, @{Name='MD5';Expression={(Get-FileHash $_.FullName -Algorithm MD5).Hash}}, @{Name='SHA256';Expression={(Get-FileHash $_.FullName -Algorithm SHA256).Hash}} | Format-Table
-Get-ChildItem -Path C:\Users\grrtt\Desktop -Recurse -File| Where {! $_.PSIsContainer} |Select-Object @{name='FileName';Expression={($_.FullName)}}, @{Name='MD5';Expression={(Get-FileHash $_.FullName -Algorithm MD5).Hash}}, @{Name='SHA256';Expression={(Get-FileHash $_.FullName -Algorithm SHA256).Hash}} | Format-Table
+#Get-ChildItem -Path C:\Users\grrtt\Desktop -Recurse -File| Where {! $_.PSIsContainer} |Select-Object @{name='FileName';Expression={($_.FullName)}}, @{Name='MD5';Expression={(Get-FileHash $_.FullName -Algorithm MD5).Hash}}, @{Name='SHA256';Expression={(Get-FileHash $_.FullName -Algorithm SHA256).Hash}} | Format-Table
 
 
 
-if ((Get-ChildItem -Path C:\Users\grrtt\Desktop -Recurse -File| Where {! $_.PSIsContainer} |Select-Object @{name='FileName';Expression={($_.FullName)}}, @{Name='MD5';Expression={(Get-FileHash $_.FullName -Algorithm MD5).Hash}}, @{Name='SHA256';Expression={(Get-FileHash $_.FullName -Algorithm SHA256).Hash}}).MD5 -eq '903429BEAA7FBEE8663345980993C1D5') {
-    Write-Host $_.FullName
-}
+#if ((Get-ChildItem -Path C:\Users\grrtt\Desktop -Recurse -File| Where {! $_.PSIsContainer} |Select-Object @{name='FileName';Expression={($_.FullName)}}, @{Name='MD5';Expression={(Get-FileHash $_.FullName -Algorithm MD5).Hash}}, @{Name='SHA256';Expression={(Get-FileHash $_.FullName -Algorithm SHA256).Hash}}).MD5 -eq '903429BEAA7FBEE8663345980993C1D5') {
+#    Write-Host $_.FullName
+#}
 
-
-Get-ChildItem -Path C:\Users\grrtt\Desktop -Recurse -File| Where {! $_.PSIsContainer} |Select-Object @{name='FileName';Expression={($_.FullName)}}, @{Name='MD5';Expression={(Get-FileHash $_.FullName -Algorithm MD5).Hash}}, @{Name='SHA256';Expression={(Get-FileHash $_.FullName -Algorithm SHA256).Hash}} | where {$_.MD5 -eq 'test' -or $_.SHA256 -eq '2671285112C2582B2FD3D6D6A45E5B5E5CA29371CFB9D6A1360EB38709937EF4'}
+#this was working
+#Get-ChildItem -Path C:\Users\grrtt\Desktop -Recurse -File| Where {! $_.PSIsContainer} |Select-Object @{name='FileName';Expression={($_.FullName)}}, @{Name='MD5';Expression={(Get-FileHash $_.FullName -Algorithm MD5).Hash}}, @{Name='SHA256';Expression={(Get-FileHash $_.FullName -Algorithm SHA256).Hash}} | where {$_.MD5 -eq 'test' -or $_.SHA256 -eq '2671285112C2582B2FD3D6D6A45E5B5E5CA29371CFB9D6A1360EB38709937EF4'}
